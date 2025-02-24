200cb4f889bdb386331851bd2dd402d1d93fc6b0
Jules Caruso <49770101+caruso1058@users.noreply.github.com>
2025-02-23 23:12:59 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FishingLogApp from "./FishingLogApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <FishingLogApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
